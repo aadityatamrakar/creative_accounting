361d856e02133d2645b70521582c00d8dcb9c3a0
Aaditya Tamrakar <aaditya.span@gmail.com>
2020-03-20 16:44:16 +0530

    asd

--- client/js/controllers/entryCtrl.js
+++ client/js/controllers/entryCtrl.js
@@ -7,7 +7,7 @@ angular
 
     $scope.report = {};
     Transaction.dashboard().$promise.then(function (report) {
-      $scope.report = report.filter(c => c.client.id == $scope.clientId)[0];
+      $scope.report = report.filter(c => c.client.id == $scope.showId)[0];
       $scope.report.hero = report.sort(function (a, b) {
         return b.payable - a.payable
       })[0].client.name;
@@ -16,7 +16,9 @@ angular
     $scope.total = {};
 
     $scope.remove = function (idx) {
-      Transaction.deleteById({id: $scope.transactions[idx].id});
+      Transaction.deleteById({
+        id: $scope.transactions[idx].id
+      });
       $scope.getData();
     }
 
@@ -50,12 +52,60 @@ angular
   .controller('TransactionController', function ($scope, ngNotify, Client, Transaction) {
     $scope.report = {};
     $scope.total = {};
-    $scope.drawChart = function () {
+    $scope.downloadimg = {
+      a: '',
+      b: '',
+      c: ''
+    }
+    // $('#transTbl').DataTable();
 
+    $scope.downloadImg = function (title) {
+      // console.log($scope.downloadimg[title]);
+      // window.open($scope.downloadimg[title]);
+      saveAs(dataURItoBlob($scope.downloadimg[title]), "charts.jpg");
+    }
+    // $scope.downloadImg = function (html_id, title) {
+    //   html2canvas($(html_id), {
+    //     onrendered: function (canvas) {
+    //       theCanvas = canvas;
+    //       document.body.appendChild(canvas);
+    //       canvas.toBlob(function (blob) {
+    //         saveAs(blob, title + ".jpg");
+    //       });
+    //     }
+    //   });
+    // }
+
+    $scope.downloadCsv = function () {
+      let headers = ['Name', 'Total Amount', 'Commission', 'Payable', 'Paid', 'Balance'];
+      let itemsFormatted = Object.values($scope.report).map(transaction => {
+        return [
+          transaction.client.name,
+          transaction.total_amount,
+          transaction.commission_amount,
+          transaction.payable_amount,
+          transaction.paying_amount,
+          (transaction.paying_amount - transaction.payable_amount)
+        ]
+      });
+      itemsFormatted.push([
+        'Total:',
+        $scope.total.total_amount,
+        $scope.total.commission_amount,
+        $scope.total.payable_amount,
+        $scope.total.paid_amount,
+        $scope.total.balance
+      ]);
+      let fileTitle = 'summary';
+      exportCSVFile(headers, itemsFormatted, fileTitle);
+    }
+
+
+    $scope.drawChart = function () {
       let rowsData = Object.values($scope.report).map(c => {
         // console.log(c);
         // return [c.client.name, c.total_amount, c.commission_amount];
-        return [c.client.name, c.total_amount];
+        return [c.client.name, c.payable_amount];
       });
       var data = google.visualization.arrayToDataTable([
         ['Name', 'Total Amount'],
@@ -63,17 +113,90 @@ angular
         ...rowsData
       ]);
       var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+        {
+          calc: "stringify",
+          sourceColumn: 1,
+          type: "string",
+          role: "annotation"
+        }
+      ]);
       var options = {
         chart: {
           title: 'Company Performance',
           subtitle: 'Sales and Commission',
         }
       };
-      chart.draw(data, google.charts.Bar.convertOptions(options));
+      google.visualization.events.addListener(chart, 'ready', function () {
+        $scope.downloadimg['a'] = chart.getImageURI();
+      });
+      chart.draw(view, google.charts.Bar.convertOptions(options));
+    }
+    $scope.drawChart2 = function () {
+      let rowsData = Object.values($scope.report).map(c => {
+        // console.log(c);
+        // return [c.client.name, c.total_amount, c.commission_amount];
+        return [c.client.name, parseFloat(((c.payable_amount / c.client.target) * 100).toFixed(2))];
+      });
+      let data = google.visualization.arrayToDataTable([
+        ['Name', 'Performance %'],
+        // ['Name', 'Total Amount', 'Commission'],
+        ...rowsData
+      ]);
+      let chart = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+        {
+          calc: "stringify",
+          sourceColumn: 1,
+          type: "string",
+          role: "annotation"
+        }
+      ]);
+      let options = {
+        chart: {
+          title: 'Company Performance %',
+          subtitle: 'Sales and Commission',
+        }
+      };
+      google.visualization.events.addListener(chart, 'ready', function () {
+        $scope.downloadimg['b'] = chart.getImageURI();
+      });
+      chart.draw(view, google.charts.Bar.convertOptions(options));
+    }
+
+    $scope.drawChart3 = function () {
+      let targetTotal = 0;
+      let rowsData = Object.values($scope.report).map(c => {
+        targetTotal += c.client.target;
+        // return [c.client.name, parseFloat(((c.payable_amount / c.client.target) * 100).toFixed(2))];
+        return [c.client.name, c.payable_amount];
+      });
+      let data = google.visualization.arrayToDataTable([
+        ['User', 'Contribution'],
+        ['Remaining', targetTotal],
+        ...rowsData
+      ]);
+      console.log(rowsData);
+      let options = {
+        title: 'Performance',
+        slices: {
+          0: { color: 'black' }
+          // 0: { color: 'yellow' },
+        }
+        // pieSliceText: 'label'
+      };
+
+      let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      google.visualization.events.addListener(chart, 'ready', function () {
+        $scope.downloadimg['c'] = chart.getImageURI();
+      });
+      chart.draw(data, options);
     }
 
     $scope.query = {
-      from_date: moment().startOf('day').toDate(),
+      from_date: moment().subtract(7, 'days').startOf('day').toDate(),
       to_date: moment().endOf('day').toDate()
     }
 
@@ -84,6 +207,8 @@ angular
       }).$promise.then(function (result) {
         $scope.report = JSON.parse(JSON.stringify(result));
         $scope.drawChart();
+        $scope.drawChart2();
+        $scope.drawChart3();
         $scope.total = Object.values($scope.report).reduce(function (acc, item) {
           acc.total_amount += item.total_amount;
           acc.commission_amount += item.commission_amount;
--- client/js/util.js
+++ client/js/util.js
@@ -110,3 +110,61 @@ function copyMessage() {
     console.log(er)
   }
 }
+
+
+function convertToCSV(objArray) {
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = '';
+  for (var i = 0; i < array.length; i++) {
+    var line = '';
+    for (var index in array[i]) {
+      if (line != '') line += ','
+      line += array[i][index];
+    }
+    str += line + '\r\n';
+  }
+  return str;
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+  if (headers) {
+    items.unshift(headers);
+  }
+
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+  var csv = this.convertToCSV(jsonObject);
+  var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+  var blob = new Blob([csv], {
+    type: 'text/csv;charset=utf-8;'
+  });
+  if (navigator.msSaveBlob) { // IE 10+
+    navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+    var link = document.createElement("a");
+    if (link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      var url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", exportedFilenmae);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+}
+
+function dataURItoBlob(dataURI) {
+  var byteString = atob(dataURI.split(',')[1]);
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  var blob = new Blob([ab], {
+    type: mimeString
+  });
+  return blob;
+}
